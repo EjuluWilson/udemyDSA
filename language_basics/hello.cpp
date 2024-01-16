8ceaebd5ce99d37281063c3cef2cff812a93d870
Wilson Ejulu <wejulu@andrew.cmu.edu>
2024-01-16 22:08:28 +0200

    update launch.json

--- language_basics/hello.cpp
+++ language_basics/hello.cpp
@@ -2,9 +2,20 @@
 
 using namespace std;
 
-int main(){
- 
-  cout<<"hello wolrd";
+/*
+reviewing c++ for DSA practice
+*/
+int main()
+{
+  // const float pi = 3.14;
+  int yourAge;
+  int yr = 2024, date = 15, month = 01;
+  cout << "hello wolrd!!" << endl;
+  cout << "This is the beginning of DSa with c++\n";
+  cout << "Today is: " << date << "-" << month << "-" << yr << endl;
+  cout << "What is your age?\n";
+  cin >> yourAge;
+  cout << "You are " << yourAge << " years old\n";
 
   return 0;
 }
