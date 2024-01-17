07df23548c352807c1b9b3f3b3e266560b596535
Wilson Ejulu <wejulu@andrew.cmu.edu>
2024-01-17 17:50:21 +0200

    done with structs

--- language_basics/hello.cpp
+++ language_basics/hello.cpp
@@ -7,24 +7,68 @@ using namespace std;
 /*
 reviewing c++ for DSA practice
 */
-int main()
-{
-  int x = 7;
-  string result = (x < 10)? "X is lessthan 10" : "it is not less than 10";
-  // string fullName;
-  // const float pi = 3.14;
-  // char a = 65, b = 104;
-  // string sch = "CMU";
-  // int yr = 2024, date = 15, month = 01;
-  // cout << "hello wolrd!!" << endl;
-  // cout << "This is the beginning of DSa with c++\n";
-  // cout << "Today is: " << date << "-" << month << "-" << yr << endl;
-  // cout << "What is your age?\n";
-  // cout<<"These are ASCIIS: " <<a<<" "<<b<<endl;
-  // cout<<"My school is: "<<sch<<endl;
-  // cout<<"enter your full name:\n";
-  // getline(cin,fullName);
-  // cout<<fullName<<endl;
-  cout<<result<<endl;
-  return 0;
-}
+// int main()
+// {
+//   int x = 7;
+//   int a = x++;
+//   int b = ++x;
+//   // string result = (x < 10) ? "X is lessthan 10" : "it is not less than 10";
+//   // string fullName;
+//   // const float pi = 3.14;
+//   // char a = 65, b = 104;
+//   // string sch = "CMU";
+//   // int yr = 2024, date = 15, month = 01;
+//   // cout << "hello wolrd!!" << endl;
+//   // cout << "This is the beginning of DSa with c++\n";
+//   // cout << "Today is: " << date << "-" << month << "-" << yr << endl;
+//   // cout << "What is your age?\n";
+//   // cout<<"These are ASCIIS: " <<a<<" "<<b<<endl;
+//   // cout<<"My school is: "<<sch<<endl;
+//   // cout<<"enter your full name:\n";
+//   // getline(cin,fullName);
+//   // cout<<fullName<<endl;
+//   cout << x << endl;
+//   cout << a << endl;
+//   cout << b << endl;
+//   return 0;
+// }
+
+//loops
+
+// int main(){
+//   int nums[6] = {1,2,3,4,5,6};
+
+//   for(int i : nums){
+//     cout<<i<<endl;
+//   }
+
+//   cout<<"The array size is: " <<sizeof(nums)<<endl;
+//   cout<<"The array val 1 size is: " <<sizeof(nums[0])<<endl;
+//   cout<<"The number of elements in the array are: " <<sizeof(nums)/sizeof(nums[0])<<endl;
+
+
+//   return 0;
+// }
+
+
+// structs
+
+// int main(){
+//   struct person{
+//     string name;
+//     int age;
+//   };
+
+//   person paul, emma;
+
+//   paul.name = "Omute Paul";
+//   paul.age = 50;
+//   emma.name = "Omoding Emmanuel";
+//   emma.age = 48;
+
+//   cout<<"eMMA's is : " <<emma.age<<endl;
+//   emma.age = 20;
+//   cout<<"eMMA's is : " <<emma.age<<endl;
+
+//   return 0;
+// }
