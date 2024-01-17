3513c9651751555631cdf6ecb93b9632f04f4685
Wilson Ejulu <wejulu@andrew.cmu.edu>
2024-01-17 13:28:08 +0200

    add comments

--- language_basics/hello.cpp
+++ language_basics/hello.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -7,15 +9,21 @@ reviewing c++ for DSA practice
 */
 int main()
 {
+  // string fullName;
   // const float pi = 3.14;
-  int yourAge;
-  int yr = 2024, date = 15, month = 01;
-  cout << "hello wolrd!!" << endl;
-  cout << "This is the beginning of DSa with c++\n";
-  cout << "Today is: " << date << "-" << month << "-" << yr << endl;
-  cout << "What is your age?\n";
-  cin >> yourAge;
-  cout << "You are " << yourAge << " years old\n";
+  // char a = 65, b = 104;
+  // string sch = "CMU";
+  // int yr = 2024, date = 15, month = 01;
+  // cout << "hello wolrd!!" << endl;
+  // cout << "This is the beginning of DSa with c++\n";
+  // cout << "Today is: " << date << "-" << month << "-" << yr << endl;
+  // cout << "What is your age?\n";
+  // cout<<"These are ASCIIS: " <<a<<" "<<b<<endl;
+  // cout<<"My school is: "<<sch<<endl;
+  // cout<<"enter your full name:\n";
+  // getline(cin,fullName);
+  // cout<<fullName<<endl;
+  cout<<round(2)<<endl;
 
   return 0;
 }
