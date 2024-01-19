a90b244b069cd053736634960588f90bae3b3ed8
Wilson Ejulu <wejulu@andrew.cmu.edu>
2024-01-19 14:37:53 +0200

    oop2

--- language_basics/oop.cpp
+++ language_basics/oop.cpp
@@ -2,26 +2,66 @@
 
 using namespace std;
 
-class Rectangle{
-
+class Rectangle
+{
+protected:
     int width = 3;
     int height = 8;
 
-    public:
-
-    int getWidth(){
-        return width;
-
+public:
+    Rectangle(int a, int b)
+    {
+        width = a;
+        height = b;
     }
-    int getHeight(){
+    int getWidth()
+    {
+        return width;
+    }
+    int getHeight()
+    {
         return height;
     }
 
+    void setWidth(int w)
+    {
+        width = w;
+    }
+    void setHeight(int h)
+    {
+        height = h;
+    }
 };
 
-int main(){
-    Rectangle a;
-    cout<<a.getHeight()<<endl;
-    cout<<a.getHeight()<<endl;
-}
+class Square : public Rectangle
+{
+public:
+    Square(int s) : Rectangle(s, s)
+    {
+        width = s;
+        height = s;
+    }
+};
 
+int main()
+{
+    Rectangle a(2, 4);
+    Rectangle b(10, 8);
+
+    cout << a.getHeight() << endl;
+    cout << a.getWidth() << endl;
+    cout << b.getHeight() << endl;
+    cout << b.getWidth() << endl;
+
+    a.setHeight(17);
+    b.setWidth(80);
+    b.setWidth(20);
+    b.setHeight(17);
+
+    cout << a.getHeight() << endl;
+    cout << a.getWidth() << endl;
+    cout << b.getHeight() << endl;
+    cout << b.getWidth() << endl;
+
+    return 0;
+}
